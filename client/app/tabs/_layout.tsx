babe13f7192d1e50748e2fe8a765ce9f9502e28e
Tabish <mohdtabishkhan001@gmail.com>
2025-03-11 01:42:47 +0530

    added icons

--- client/app/tabs/_layout.tsx
+++ client/app/tabs/_layout.tsx
@@ -1,6 +1,12 @@
 import { Tabs } from "expo-router";
+import {
+	ClipboardList,
+	House,
+	MapPlus,
+	Search,
+	Settings,
+} from "lucide-react-native";
 import React from "react";
-
 const TabsLayout = () => {
 	return (
 		<Tabs screenOptions={{ headerShown: false }}>
@@ -9,6 +15,13 @@ const TabsLayout = () => {
 				options={{
 					title: "Home",
 					headerShown: false,
+					tabBarIcon: ({ focused }) => (
+						<House
+							className="flex flex-1 w-full h-full"
+							size={30}
+							color={focused ? "#7e7e7e" : "#000000"}
+						/>
+					),
 				}}
 			/>
 			<Tabs.Screen
@@ -16,6 +29,13 @@ const TabsLayout = () => {
 				options={{
 					title: "Trips",
 					headerShown: false,
+					tabBarIcon: ({ focused }) => (
+						<ClipboardList
+							className="flex flex-1 w-full h-full"
+							size={30}
+							color={focused ? "#90d6ff" : "#000000"}
+						/>
+					),
 				}}
 			/>
 			<Tabs.Screen
@@ -23,6 +43,13 @@ const TabsLayout = () => {
 				options={{
 					title: "CreateANewTrip",
 					headerShown: false,
+					tabBarIcon: ({ focused }) => (
+						<MapPlus
+							className="flex flex-1 w-full h-full"
+							size={30}
+							color={focused ? "#90d6ff" : "#000000"}
+						/>
+					),
 				}}
 			/>
 			<Tabs.Screen
@@ -30,6 +57,13 @@ const TabsLayout = () => {
 				options={{
 					title: "Search",
 					headerShown: false,
+					tabBarIcon: ({ focused }) => (
+						<Search
+							className="flex flex-1 w-full h-full"
+							size={30}
+							color={focused ? "#90d6ff" : "#000000"}
+						/>
+					),
 				}}
 			/>
 			<Tabs.Screen
@@ -37,6 +71,13 @@ const TabsLayout = () => {
 				options={{
 					title: "Settings",
 					headerShown: false,
+					tabBarIcon: ({ focused }) => (
+						<Settings
+							className="flex flex-1 w-full h-full"
+							size={30}
+							color={focused ? "#90d6ff" : "#000000"}
+						/>
+					),
 				}}
 			/>
 		</Tabs>
--- client/app/tabs/home.tsx
+++ client/app/tabs/home.tsx
@@ -5,7 +5,7 @@ import { Text, View } from "react-native";
 export default function Home() {
 	return (
 		<View className="  flex-1  justify-center items-center">
-			<Text className=" font-bold text-3xl ">Create A new trip</Text>
+			<Text className=" font-bold text-3xl ">Home</Text>
 		</View>
 	);
 }
